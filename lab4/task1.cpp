c00ccc95d7e35857fa2e89215c6f8a74e4289192
USER <msobanakram7@gmail.com>
2023-10-09 22:02:06 +0500

    after binary search

--- lab4/task1.cpp
+++ lab4/task1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int sumN(int n);
@@ -11,6 +12,11 @@ int dividedby3(int n);
 int dividedby2(int n);
 string decimalToBinary(int n);
 string integerToString(int n);
+int GCD(int a, int b);
+int searchFirstIndex(int* array, int start, int val);
+int searchLastIndex(int* array, int start, int val);
+int binarySearch(int* array, int start, int end, int mid, int val);
+int power(int a, int b);
 
 
 int main()
@@ -40,6 +46,24 @@ int main()
 
     // // integer to string
     // cout << integerToString(54353) << endl;
+
+    // GCD
+    // int a = 13, b = 48;
+    // cout <<"GCD is : " << GCD(a, b); 
+
+    // search First Index
+    // int array[] = {1,2,3,4,5,6,7,8,9,10};
+    // cout << "First Index is : " << searchFirstIndex(array, 0, 9);
+
+    // search last Index
+    // cout << "Last index is : " << searchLastIndex(array, sizeof(array), 9);
+
+    // binary search
+    // cout << "Index is : " << binarySearch(array, 0, sizeof(array), 0, 0);
+
+    // power 0(y)
+    cout << power(2, 3);
+
 }
 
 
@@ -59,7 +83,6 @@ int sumOdd(int n)
     }
     return n + sumOdd(n-2);
 }
-
 double sumPower2(int n)
 {
     if (n == 0)
@@ -100,7 +123,6 @@ string decimalToBinary(int n)
     }
     return decimalToBinary(n / 2) + ((n % 2 == 0) ? "0" : "1"); 
 }
-
 string integerToString(int n)
 {
     if (n < 10)
@@ -108,4 +130,72 @@ string integerToString(int n)
         return to_string(n);
     }
     return integerToString(n / 10) + (to_string(n % 10));
+}
+int GCD(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else 
+    {
+        return GCD(b, a % b);
+    }
+}
+int searchFirstIndex(int* array, int start, int val)
+{
+    if (array[start] == val)
+    {
+        return start;
+    }
+    else 
+    {
+        return searchFirstIndex(array, start + 1, val);
+    }
+}
+int searchLastIndex(int* array, int start, int val)
+{
+    if (array[start] == val)
+    {
+        return start;
+    }
+    else 
+    {
+        return searchLastIndex(array, start - 1, val);
+    }
+}
+int binarySearch(int* array, int start, int end, int mid, int val)
+{
+    mid = start + (end - start) / 2;
+    if (array[mid] == val)
+    {
+        return mid;
+    }
+    else if (start + 1 == end)
+    {
+        return -1;
+    }
+    else if (array[mid] > val)
+    {
+        end = mid;
+    }
+    else if (array[mid] < val)
+    {
+        start = mid;
+    }
+    
+    return binarySearch(array, start, end, mid, val);
+    
+}
+int power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    return a * power(a, b - 1);
+}
+int powerLog(int a, int b)
+{
+    
 }
